15a55f6fa9fc1fb48f50570bce89662257c8c8ea
k452 <55301839+k452@users.noreply.github.com>
2021-12-21 01:30:52 +0900

    注釈

--- main.cpp
+++ main.cpp
@@ -5,22 +5,33 @@
 
 using namespace std;
 
-const int TIMES = 10000000;
-int result = 0;
-
 int main()
 {
-  std::chrono::system_clock::time_point start, end; // 型は auto で可
-  start = std::chrono::system_clock::now();         // 計測開始時間
+  // 時間計測開始
+  std::chrono::system_clock::time_point start, end;
+  start = std::chrono::system_clock::now();
 
+  // 乱数シード
   std::srand(time(NULL));
+
+  // ループ回数
+  const int TIMES = 10000000;
+
+  // ループ実行
+  int result = rand();
   for (int i = 0; i < TIMES; i++)
   {
     result ^= rand();
+    result <<= 1;
+    result >>= 1;
+    result ^= rand();
   }
-  end = std::chrono::system_clock::now();                                                      // 計測終了時間
-  double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); //処理に要した時間をミリ秒に変換
+
+  // 時間計測終了
+  end = std::chrono::system_clock::now();
+  double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+  // 時間出力
   cout << "所要時間：" << elapsed << "ミリ秒" << endl;
-  // cout << hex << result << endl;
   return 0;
 }
